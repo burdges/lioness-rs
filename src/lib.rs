d58c4d2239c1846fb5561e4964ce5a1cdc558d7f
Jeffrey Burdges <burdges@gnunet.org>
2016-12-16 03:13:21 +0100

    Remove associated constants that Rust des not handle correctly

--- src/lib.rs
+++ src/lib.rs
@@ -46,20 +46,17 @@ pub trait LionessCipher {
 */
 
 
-/// Add associated type missing from `crypto::digest::Digest`
-/// in rust-crypto.
-pub trait DigestLioness: Digest {
-    const ResultSize: usize;
-    const KeySize: usize;
+pub const DigestResultSize: usize = 32;
+pub const DigestKeySize: usize = 64;
 
-    fn new_digestlioness(k: &[u8; KeySize]) -> Self;
+/// Adapt a given `crypto::digest::Digest` to Lioness.
+pub trait DigestLioness: Digest {
+    fn new_digestlioness(k: &[u8]) -> Self;
 }
 
 impl DigestLioness for Blake2b {
-    const ResultSize: usize = 64;
-    const KeySize: usize = 64;
-    fn new_digestlioness(k: &[u8; KeySize]) -> Self {
-        Blake2b::new_keyed(64,k)
+    fn new_digestlioness(k: &[u8]) -> Self {
+        Blake2b::new_keyed(DigestResultSize,k)
     }
 }
 
@@ -71,33 +68,32 @@ impl DigestLioness for Sha3 {
 }
 */
 
+const StreamCipherKeySize: usize = 32;
 
 /// Add associated type missing from rust-crypto lacks's trait
 /// `crypto::symmetriccipher::SynchronousStreamCipher`
 pub trait StreamCipherLioness: SynchronousStreamCipher {
-    const KeySize: usize;
-    fn new_streamcipherlioness(k: &[u8; KeySize]) -> Self;
+    fn new_streamcipherlioness(k: &[u8]) -> Self;
 }
 
 impl StreamCipherLioness for ChaCha20 {
-    const KeySize: usize = 64;
-    fn new_streamcipherlioness(k: &[u8; KeySize]) -> ChaCha20 {
+    fn new_streamcipherlioness(k: &[u8]) -> ChaCha20 {
         ChaCha20::new(k, &[0u8;12])
     }
 }
-
-// H::Key = <H as DigestLioness>::Key
-// SC::Key = <SC as StreamCipherLioness>::Key
 
 
 /// Lioness implemented generically over a Digest and StreamCipher
 pub struct Lioness<H,SC>
   where H: DigestLioness+Digest, 
         SC: StreamCipherLioness+SynchronousStreamCipher {
-    k1: [u8; <SC as StreamCipherLioness>::KeySize],
-    k2: [u8; <H as DigestLioness>::KeySize],
-    k3: [u8; <SC as StreamCipherLioness>::KeySize],
-    k4: [u8; <H as DigestLioness>::KeySize],
+    k1: [u8; StreamCipherKeySize],
+    k2: [u8; DigestKeySize],
+    k3: [u8; StreamCipherKeySize],
+    k4: [u8; DigestKeySize],
+    // I dislike these as H and SC should not impact variance.
+    h: std::marker::PhantomData<H>,
+    sc: std::marker::PhantomData<SC>,
 }
 
 impl<H,SC> Lioness<H,SC>
@@ -105,9 +101,11 @@ impl<H,SC> Lioness<H,SC>
         SC: StreamCipherLioness+SynchronousStreamCipher
 {
     fn encrypt(&self, block: &mut [u8]) -> Result<(), LionessError> {
-        let mut hr: [u8; <H as DigestLioness>::ResultSize];
-        let mut k: <SC as StreamCipherLioness>::Key;
-        let keylen = std::mem::size_of_val(k);
+        debug_assert!(DigestResultSize == StreamCipherKeySize);
+        let mut hr: [u8; DigestResultSize];
+        let mut k: [u8; StreamCipherKeySize];
+        let keylen = std::mem::size_of_val(&k);
+        assert!(keylen == 32);
 
         let blocklen = block.len();
         // assert!(len > keylen);
@@ -124,26 +122,26 @@ impl<H,SC> Lioness<H,SC>
         unsafe { tmp_right.set_len(blocklen-keylen); }
 
         // R = R ^ S(L ^ K1)
-        xor(left, &self.k1, &k);
+        xor(left, &self.k1, &mut k);
         let mut sc = SC::new_streamcipherlioness(&k);
         sc.process(right, tmp_right.as_mut_slice());
 
         // L = L ^ H(K2, R)
         let mut h = H::new_digestlioness(&self.k2);
         h.input(tmp_right.as_slice());
-        h.result(&hr);
-        xor_assign(left,&hr[0..keylen]);
+        h.result(&mut hr);
+        xor_assign(left,&hr);
 
         // R = R ^ S(L ^ K3)
-        xor(left, &self.k3, &k);
+        xor(left, &self.k3, &mut k);
         let mut sc = SC::new_streamcipherlioness(&k);
         sc.process(tmp_right.as_slice(), right);
 
         // L = L ^ H(K4, R)
         let mut h = H::new_digestlioness(&self.k2);
         h.input(tmp_right.as_slice());
-        h.result(&hr);
-        xor_assign(left,&hr[0..keylen]);
+        h.result(&mut hr);
+        xor_assign(left,&hr);
 
         Ok(())
     }
