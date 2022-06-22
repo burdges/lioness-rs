1a83815dc582ebf3131da59b3f16e781626a5973
vnpmid <dhani.prg.2007@gmail.com>
2022-06-22 21:51:37 +0700

    Add LionessBlake3

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 // Copyright 2016 Jeffrey Burdges and David Stainton
 
 //! Lioness wide block cipher
-
 #[macro_use]
 extern crate arrayref;
 
 extern crate chacha;
 extern crate blake2;
+extern crate digest;
 extern crate keystream;
+extern crate blake3;
 
 #[cfg(test)]
 extern crate rand;
@@ -17,8 +18,10 @@ extern crate hex;
 
 use keystream::KeyStream;
 use chacha::ChaCha;
-use blake2::{Blake2bMac, digest::{KeyInit, Update, FixedOutput, consts::U32}};
-use std::convert::TryInto;
+use blake2::Blake2bMac;
+use blake3::Hasher;
+use digest::{KeyInit, Update, FixedOutput, consts::U32};
+use std::{io::Read, convert::TryInto};
 
 pub mod error;
 pub use error::LionessError;
@@ -27,12 +30,16 @@ pub use util::{xor, xor_assign};
 
 pub const DIGEST_RESULT_SIZE: usize = 32;
 pub const DIGEST_KEY_SIZE: usize = 64;
+pub const BLAKE3_RESULT_SIZE: usize = 32;
+pub const BLAKE3_KEY_SIZE: usize = 32;
 pub const STREAM_CIPHER_KEY_SIZE: usize = 32;
 pub const RAW_KEY_SIZE: usize = 2*STREAM_CIPHER_KEY_SIZE + 2*DIGEST_KEY_SIZE;
+pub const RAW_BLAKE3_KEY_SIZE: usize = 2*STREAM_CIPHER_KEY_SIZE + 2*BLAKE3_KEY_SIZE;
 const CHACHA20_NONCE_SIZE: usize = 8;
 
 // Type alias for Blake2bMac256 for compatibility with old lioness
 pub type Blake2bMac256 = Blake2bMac<U32>;
+pub type Blake3Hasher = Hasher;
 
 /// Adapt a given `crypto::digest::Digest` to Lioness.
 pub trait DigestLioness : KeyInit+Update+FixedOutput {
@@ -41,45 +48,70 @@ pub trait DigestLioness : KeyInit+Update+FixedOutput {
 	
 }
 
+pub trait Blake3 {
+	
+    fn new_blake3(key: &[u8; BLAKE3_KEY_SIZE]) -> Blake3Hasher;
+	
+}
+
 impl DigestLioness for Blake2bMac256 {
 	
-    fn new_digest_lioness(key: &[u8]) -> Self {
+    fn new_digest_lioness(key: &[u8]) -> Blake2bMac256 {
         Blake2bMac256::new_from_slice(&key).unwrap()
     }
 	
 }
 
+impl Blake3 for Blake3Hasher {
+	
+    fn new_blake3(key: &[u8; BLAKE3_KEY_SIZE]) -> Blake3Hasher {
+        Blake3Hasher::new_keyed(key)
+    }
+	
+}
 
-/// Adapt a given `crypto::symmetriccipher::KeyStream`
-/// to lioness.
 pub trait StreamCipherLioness : KeyStream {
+	
     fn new_streamcipher_lioness(k: &[u8; STREAM_CIPHER_KEY_SIZE]) -> Self;
+	
 }
 
 impl StreamCipherLioness for ChaCha {
+	
     fn new_streamcipher_lioness(k: &[u8; STREAM_CIPHER_KEY_SIZE]) -> ChaCha {
-        // here we intentionally initialize the ChaCha20 stream cipher with
-        // an 8 byte zero nonce which is sufficient in the context of Lioness
         ChaCha::new_chacha20(k, &[0u8;CHACHA20_NONCE_SIZE])
     }
+	
 }
 
-
-/// Lioness implemented generically over a Digest and StreamCipher
 pub struct Lioness<H,SC>
-where H: DigestLioness+KeyInit+Update+FixedOutput, 
-      SC: StreamCipherLioness+KeyStream {
-    k1: [u8; STREAM_CIPHER_KEY_SIZE],
-    k2: [u8; DIGEST_KEY_SIZE],
-    k3: [u8; STREAM_CIPHER_KEY_SIZE],
-    k4: [u8; DIGEST_KEY_SIZE],
-    h: std::marker::PhantomData<H>,
-    sc: std::marker::PhantomData<SC>,
-}
+where 
+	H: DigestLioness+KeyInit+Update+FixedOutput, 
+    SC: StreamCipherLioness+KeyStream {
+		k1: [u8; STREAM_CIPHER_KEY_SIZE],
+		k2: [u8; DIGEST_KEY_SIZE],
+		k3: [u8; STREAM_CIPHER_KEY_SIZE],
+		k4: [u8; DIGEST_KEY_SIZE],
+		h: std::marker::PhantomData<H>,
+		sc: std::marker::PhantomData<SC>,
+	}
+
+pub struct LionessBlake3<H,SC>
+where
+	H: Blake3,
+	SC: StreamCipherLioness+KeyStream {
+		k1: [u8; STREAM_CIPHER_KEY_SIZE],
+		k2: [u8; BLAKE3_KEY_SIZE],
+		k3: [u8; STREAM_CIPHER_KEY_SIZE],
+		k4: [u8; BLAKE3_KEY_SIZE],
+		h: std::marker::PhantomData<H>,
+		sc: std::marker::PhantomData<SC>,
+	}
 
 impl<H,SC> Lioness<H,SC>
-where H: DigestLioness+KeyInit+Update+FixedOutput,
-      SC: StreamCipherLioness+KeyStream
+where 
+	H: DigestLioness+KeyInit+Update+FixedOutput,
+    SC: StreamCipherLioness+KeyStream
 {
     pub fn encrypt(&self, block: &mut [u8]) -> Result<(), LionessError> {
         debug_assert!(DIGEST_RESULT_SIZE == STREAM_CIPHER_KEY_SIZE);
@@ -173,12 +205,109 @@ where H: DigestLioness+KeyInit+Update+FixedOutput,
     }
 }
 
-pub type LionessDefault = Lioness<Blake2bMac256,ChaCha>;
+impl<H,SC> LionessBlake3<H,SC>
+where
+	H: Blake3,
+	SC: StreamCipherLioness+KeyStream
+{
+    pub fn encrypt(&self, block: &mut [u8]) -> Result<(), LionessError> {
+        debug_assert!(BLAKE3_RESULT_SIZE == STREAM_CIPHER_KEY_SIZE);
+        let mut hr = [0u8; BLAKE3_RESULT_SIZE];
+        let mut k = [0u8; STREAM_CIPHER_KEY_SIZE];
+        let keylen = std::mem::size_of_val(&k);
+        debug_assert!(keylen == 32);
 
+        let blocklen = block.len();
+        if blocklen <= keylen {
+            return Err(LionessError::BlockSizeError)
+        }
+
+        let (left,right) : (&mut [u8],&mut [u8]) = block.split_at_mut(keylen);
+
+        // R = R ^ S(L ^ K1)
+        xor(left, &self.k1, &mut k);
+        let mut sc = SC::new_streamcipher_lioness(&k);
+        sc.xor_read(right) ?;
+
+        // L = L ^ H(K2, R)
+        let mut h = H::new_blake3(&self.k2);
+        h.update(&mut *right);
+        h.finalize_xof().read(&mut hr).unwrap();
+		xor_assign(left,&hr);
+
+        // R = R ^ S(L ^ K3)
+        xor(left, &self.k3, &mut k);
+        let mut sc = SC::new_streamcipher_lioness(&k);
+        sc.xor_read(right) ?;
+
+        // L = L ^ H(K4, R)
+        let mut h = H::new_blake3(&self.k4);
+        h.update(&mut *right);
+        h.finalize_xof().read(&mut hr).unwrap();
+		xor_assign(left,&hr);
+
+        Ok(())
+    }
+
+    pub fn decrypt(&self, block: &mut [u8]) -> Result<(), LionessError> {
+        debug_assert!(BLAKE3_RESULT_SIZE == STREAM_CIPHER_KEY_SIZE);
+        let mut hr = [0u8; BLAKE3_RESULT_SIZE];
+        let mut k = [0u8; STREAM_CIPHER_KEY_SIZE];
+        let keylen = std::mem::size_of_val(&k);
+        debug_assert!(keylen == 32);
+
+        let blocklen = block.len();
+	    if blocklen <= keylen {
+            return Err(LionessError::BlockSizeError)
+        }
+
+        let (left,right) : (&mut [u8],&mut [u8]) = block.split_at_mut(keylen);
+
+        // L = L ^ H(K4, R)
+        let mut h = H::new_blake3(&self.k4);
+        h.update(&mut *right);
+        h.finalize_xof().read(&mut hr).unwrap();
+		xor_assign(left,&hr);
+
+        // R = R ^ S(L ^ K3)
+        xor(left, &self.k3, &mut k);
+        let mut sc = SC::new_streamcipher_lioness(&k);
+        sc.xor_read(right) ?;
+
+        // L = L ^ H(K2, R)
+        let mut h = H::new_blake3(&self.k2);
+        h.update(&mut *right);
+		h.finalize_xof().read(&mut hr).unwrap();
+		xor_assign(left,&hr);
+
+        // R = R ^ S(L ^ K1)
+        xor(left, &self.k1, &mut k);
+        let mut sc = SC::new_streamcipher_lioness(&k);
+        sc.xor_read(right) ?;
+
+        Ok(())
+    }
+
+    /// Given a key, create a new Lioness cipher
+    pub fn new_raw(key: &[u8; 2*STREAM_CIPHER_KEY_SIZE + 2*BLAKE3_KEY_SIZE]) -> LionessBlake3<H,SC> {
+        let (_k1,_k2,_k3,_k4) = array_refs![key,STREAM_CIPHER_KEY_SIZE,BLAKE3_KEY_SIZE,STREAM_CIPHER_KEY_SIZE,BLAKE3_KEY_SIZE];
+        LionessBlake3 {
+            k1: *_k1,
+            k2: *_k2,
+            k3: *_k3,
+            k4: *_k4,
+            h: std::marker::PhantomData,
+            sc: std::marker::PhantomData,
+        }
+    }
+}
+
+pub type LionessDefault = Lioness<Blake2bMac256,ChaCha>;
 
 #[cfg(test)]
 mod tests {
     use rand::prelude::*;
+	use digest::generic_array::GenericArray;
     use super::*;
 
     struct Test {
@@ -193,6 +322,24 @@ mod tests {
         let mut key = [0u8; RAW_KEY_SIZE];
         thread_rng().fill_bytes(&mut key);
         let l = Lioness::<Blake2bMac256,ChaCha>::new_raw(&key);
+        let mut v: Vec<u8> = TEST_PLAINTEXT.to_owned();
+        assert_eq!(v,TEST_PLAINTEXT);
+        l.encrypt(&mut v).unwrap();
+        assert_eq!(v.len(),TEST_PLAINTEXT.len());
+        l.decrypt(&mut v).unwrap();
+        assert_eq!(v,TEST_PLAINTEXT);
+        l.decrypt(&mut v).unwrap();
+        assert_eq!(v.len(),TEST_PLAINTEXT.len());
+        l.encrypt(&mut v).unwrap();
+        assert_eq!(v,TEST_PLAINTEXT);
+    }
+	
+	#[test]
+    fn simple_encrypt_decrypt_blake3_test() {
+        const TEST_PLAINTEXT: &'static [u8] = b"Hello there world, I'm just a test string";
+        let mut key = [0u8; RAW_BLAKE3_KEY_SIZE];
+        thread_rng().fill_bytes(&mut key);
+        let l = LionessBlake3::<Blake3Hasher,ChaCha>::new_raw(&key);
         let mut v: Vec<u8> = TEST_PLAINTEXT.to_owned();
         assert_eq!(v,TEST_PLAINTEXT);
         l.encrypt(&mut v).unwrap();
@@ -255,5 +402,29 @@ mod tests {
              8b2c890ca0405c957aa277d").unwrap();
 
         test_cipher(&[ Test {key,input,output} ]);
+    }
+	
+	#[test]
+    fn encryption_is_reciprocal_to_decryption_for_chacha20_blake3_variant() {
+        let key = GenericArray::from(b"my-awesome-key-that-is-perfect-length-to-work-with-chacha20-and-blake3-lioness-cipher-after-adding-a-little-bit-of-extra-padding".to_owned()).as_slice().try_into().expect("slice with incorrect length");
+		let l = LionessBlake3::<Blake3Hasher,ChaCha>::new_raw(&key);
+        let data = b"Hello there! This is some test data that has length at least as long as the digest size of Blake3.";
+        let mut block = *data;
+		l.encrypt(&mut block).unwrap();
+		assert_ne!(data.to_vec(), block.to_vec());
+		l.decrypt(&mut block).unwrap();
+		assert_eq!(data.to_vec(), block.to_vec());
+    }
+	
+	#[test]
+    fn encryption_is_reciprocal_to_decryption_for_block_chacha20_blake3_variant() {
+        let key = GenericArray::from(b"my-awesome-key-that-is-perfect-length-to-work-with-chacha20-and-blake3-lioness-cipher-after-adding-a-little-bit-of-extra-padding".to_owned()).as_slice().try_into().expect("slice with incorrect length");
+		let l = LionessBlake3::<Blake3Hasher,ChaCha>::new_raw(&key);
+        let data = b"This is some test data of the same length as specified blockSize".to_owned();
+        let mut block = GenericArray::from(data);
+		l.encrypt(&mut block).unwrap();
+        assert_ne!(data.to_vec(), block.to_vec());
+		l.decrypt(&mut block).unwrap();
+        assert_eq!(data.to_vec(), block.to_vec());
     }
 } // tests
